c3ce5497d63cea3ee05393f8bc678f5f565ba376
ben-altman <60247694+ben-altman@users.noreply.github.com>
2021-10-14 00:22:16 -0400

    commit tree 98ab5dcb7594f60018b8a87218b53f2828888c97

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 
+const superbowlWin = (record) => {
+    const win = record.find(function(r) {return r.result === "W"})
+    if (win) {
+        return win.year
+    } else {
+        return undefined
+    }
+}
+
+// record.find(function(r) {return r.result === "W"});
